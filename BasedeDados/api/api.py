032562a804de467dc2086e4d281cf2dfd8c3aa6e
Paulo Mendes <itzfs233@gmail.com>
2025-05-05 19:34:26 +0100

    bora

--- BasedeDados/api/api.py
+++ BasedeDados/api/api.py
@@ -1,41 +1,7 @@
-import os
-from flask import Flask, jsonify, request, session, make_response
-import psycopg2
-import jwt
-from datetime import datetime, timedelta, timezone
-from dotenv import load_dotenv
-from itsdangerous import Serializer, BadSignature, SignatureExpired
-
-load_dotenv()
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-
-
-
-def get_db_connection():
-    conn = psycopg2.connect(
-        dbname=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT')
-    )
-    return conn
-
-@app.route('/', methods=["GET"])
+@app.route('/')
 def home():
-    return "Welcome to bijbhknk!"
-
-
-if __name__ == '__main__':
-    print("Starting Flask server...")
-    
-
-
-    print("quero ir tomar banho")
-    app.run(debug=False)
-
-
-   
+    return jsonify({"message": "Welcome to bijbhknk!"})
