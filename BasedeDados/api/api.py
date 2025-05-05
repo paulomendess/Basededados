3a77c96e6810e78642ad7444041f88bf8b032541
Paulo Mendes <itzfs233@gmail.com>
2025-05-05 20:19:30 +0100

    la

--- BasedeDados/api/api.py
+++ BasedeDados/api/api.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Welcome to bijbhknk!"})
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
